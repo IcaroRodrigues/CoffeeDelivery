0a2eeccb14f3373dff3efe1b9e7f41ba9c73dc68
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2023-04-14 19:43:42 -0300

    add: font roboto to project

--- frontend/src/pages/alguma_pagina.tsx
+++ frontend/src/pages/alguma_pagina.tsx
@@ -1,8 +1,8 @@
 export const Pagina = () => {
 
   return (
-    <div className='flex w-auto h-screen items-center justify-center bg-zinc-800'>
-      <h1 className='text-zinc-400 first-letter text-5xl'>THE SEALS</h1>
+    <div className='flex h-screen items-center justify-center bg-zinc-800'>
+      <h1 className='text-zinc-400 text-5xl'>THE SEALS</h1>
     </div>
   )
 }
